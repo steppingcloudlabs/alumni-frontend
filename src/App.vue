7d1f24c939cad5e74e5dd04a7a7a8c9d7f6ab7f8
Prakritidev Verma <prakritidevverma@gmail.com>
2019-08-28 10:43:25 +0530

    - [1] feat: home page

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <Header></Header>
+    <NavigationDrawer></NavigationDrawer>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+import NavigationDrawer from './components/NavigationDrawer';
+import Header from './components/Header';
+
+export default {
+  name: 'App',
+  components: {
+    NavigationDrawer,
+    Header,
+  },
+  data: () => ({
+    //
+  }),
+  mounted() {
+    this.$store.dispatch('authenticate');
+  }
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-layout row wrap px-5 py-2 class="elevation-9">
+    <v-flex xs4>
+      <img :src="companyData.titan.companyLogo" height="50" />
+    </v-flex>
+    <v-flex xs6>
+      <p class="text-center vertical-align-content font-weight-light">Alumni Portal</p>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import data from "@/assets/data";
+
+export default {
+  data() {
+    return {
+      companyData: data
+    };
+  },
+  created() {},
+  methods: {}
+};
+</script>
+
+<style>
+.vertical-align-content {
+  position: relative;
+  top: 50%;
+  transform: translateY(-50%);
+}
+</style>
