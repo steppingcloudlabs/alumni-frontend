e1915297a91af961bf911f8a28d993ce7a6e4630
darak-a <darakshaabid25@gmail.com>
2021-01-15 16:50:49 +0530

    minor update

--- src/components/core/mesgcard.vue
+++ src/components/core/mesgcard.vue
@@ -1,37 +1,54 @@
 <template>
- <v-parallax
+  <v-parallax
     height="600"
-    style="width:100%"
+    style="width: 100%"
     src="@/assets/shutterstock/msg.jpg"
   >
-<v-overlay :absolute="true" color="black">
-  <v-card flat tile min-height="550px" color="transparent"  style="padding-top:3%;padding-bottom:5%;" >
-    <v-card-title >
-      <p class=" font-weight-bold"  style="margin:auto; font-size:25px">Message from Director's Desk</p>
-    </v-card-title>
-    <v-layout row wrap style="padding-top:3%;">
-      <v-flex xs4 sm4 md4 lg4 xl4>
-        <div style="margin-left:18%; ">
-        <v-img  style="border-radius:10%" height="350px" width="300px" src="@/assets/director.jpg"></v-img>
-      </div>
-      </v-flex>
-      
-      <v-flex xs8 sm8 md8 lg8 xl8>
-        <v-card max-width="100%" elevation="0" color="transparent" style="margin-left:3%;margin-right:6%">
-          <!-- <v-card-title >
+    <v-overlay :absolute="true" color="black">
+      <v-card
+        flat
+        tile
+        min-height="550px"
+        color="transparent"
+        style="padding-top: 3%; padding-bottom: 5%"
+      >
+        <v-card-title>
+          <p class="font-weight-bold" style="margin: auto; font-size: 25px">
+            Message from Director's Desk
+          </p>
+        </v-card-title>
+        <v-layout row wrap style="padding-top: 3%">
+          <v-flex xs4 sm4 md4 lg4 xl4>
+            <div style="margin-left: 18%">
+              <v-img
+                style="border-radius: 10%"
+                height="350px"
+                width="300px"
+                src="@/assets/director1.png"
+              ></v-img>
+            </div>
+          </v-flex>
+
+          <v-flex xs8 sm8 md8 lg8 xl8>
+            <v-card
+              max-width="100%"
+              elevation="0"
+              color="transparent"
+              style="margin-left: 3%; margin-right: 6%"
+            >
+              <!-- <v-card-title >
             <span class="title font-weight-light" style="margin-top:-25px;">Testing Heading</span>
           </v-card-title> -->
 
-          <v-card-text style="margin-top:5%;color:white;width:70%">
-            
-          {{message}}
-          </v-card-text>
-        </v-card>
-      </v-flex>
-    </v-layout>
-  </v-card>
-</v-overlay>
- </v-parallax>
+              <v-card-text style="margin-top: 5%; color: white; width: 70%">
+                {{ message }}
+              </v-card-text>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-card>
+    </v-overlay>
+  </v-parallax>
 
   <!-- <div class="d-flex flex-no-wrap justify-space-between" style="margin: 0 100px;">
     <div>
